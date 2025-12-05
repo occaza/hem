124799dd4fd3c6d42fe1f6cf4b6a6f010477e9a6
occaza <alenaproject.it@gmail.com>
2025-12-06 05:33:17 +0700

    chore: revert hardcoded bot username

--- src/lib/components/auth/TelegramLoginButton.svelte
+++ src/lib/components/auth/TelegramLoginButton.svelte
@@ -43,8 +43,7 @@
 		// Inject script
 		const script = document.createElement('script');
 		script.src = 'https://telegram.org/js/telegram-widget.js?22';
-		// HARDCODED FOR DEBUGGING
-		script.setAttribute('data-telegram-login', 'bebboot_bot');
+		script.setAttribute('data-telegram-login', PUBLIC_TELEGRAM_BOT_USERNAME);
 		script.setAttribute('data-size', 'large');
 		script.setAttribute('data-radius', '10');
 		script.setAttribute('data-onauth', 'onTelegramAuth(user)');
